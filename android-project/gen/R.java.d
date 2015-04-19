55683859ee2923d3cd74b90387625b5111f032ca
Juliano-Mac <julianobsg@gmail.com>
2015-04-19 13:00:21 -0300

    adicionando o gradle e build settings e removendo o box 2d por falta de tempo para usá-lo

---
+++ android-project/gen/R.java.d
@@ -0,0 +1,12 @@
+/Users/mac/Documents/AGameEngine/android-project/gen/org/libsdl/app/R.java \
+ : /Users/mac/Documents/AGameEngine/android-project/res/drawable-hdpi/ic_launcher.png \
+/Users/mac/Documents/AGameEngine/android-project/res/drawable-mdpi/ic_launcher.png \
+/Users/mac/Documents/AGameEngine/android-project/res/drawable-xhdpi/ic_launcher.png \
+/Users/mac/Documents/AGameEngine/android-project/res/drawable-xxhdpi/ic_launcher.png \
+/Users/mac/Documents/AGameEngine/android-project/res/layout/main.xml \
+/Users/mac/Documents/AGameEngine/android-project/res/values/strings.xml \
+/Users/mac/Documents/AGameEngine/android-project/bin/res/drawable-hdpi/ic_launcher.png \
+/Users/mac/Documents/AGameEngine/android-project/bin/res/drawable-mdpi/ic_launcher.png \
+/Users/mac/Documents/AGameEngine/android-project/bin/res/drawable-xhdpi/ic_launcher.png \
+/Users/mac/Documents/AGameEngine/android-project/bin/res/drawable-xxhdpi/ic_launcher.png \
+/Users/mac/Documents/AGameEngine/android-project/bin/AndroidManifest.xml \
---
+++ bin/agameengine.sh
@@ -0,0 +1,15 @@
+echo "Executing a game engine"
+#echo `pwd `
+#echo `type -a newagameengine.sh`
+dirname=`perl -e 'use Cwd "abs_path";print abs_path(shift)' $0`
+#echo $dirname
+
+scriptDir="$(cd "$(dirname "$0")" && pwd -P)"
+#echo $scriptDir
+
+gradle="$scriptDir/gradlew"
+
+buildFile="$scriptDir/build.gradle"
+#echo $gradle
+#$gradle tasks
+$gradle -q -b $buildFile -Pparam=$2 $1
